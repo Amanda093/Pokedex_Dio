58bab55df945ed5ccede2ed9875a5f2289e368b9
Amanda093 <vovigadagamer@gmail.com>
2024-07-25 22:34:31 -0300

    primeiro commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,16 @@ const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
 const maxRecords = 151
-const limit = 10
+const limit = 12
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
+            <div class="title">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+            </div>
 
             <div class="detail">
                 <ol class="types">
@@ -19,6 +21,10 @@ function convertPokemonToLi(pokemon) {
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
             </div>
+            
+            <button id="showInfoButton" type="button">
+                Show Info
+            </button>
         </li>
     `
 }
--- assets/js/pokeApi.js
+++ assets/js/pokeApi.js
@@ -1,4 +1,3 @@
-
 const pokeApi = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
@@ -7,22 +6,19 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     pokemon.name = pokeDetail.name
 
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    const [type] = types
 
+    const [type] = types
     pokemon.types = types
     pokemon.type = type
 
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
-
     return pokemon
 }
-
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
         .then(convertPokeApiDetailToPokemon)
 }
-
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
